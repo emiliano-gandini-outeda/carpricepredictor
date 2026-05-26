4aa725b68d3f6f4d12735dd5fe1fd0008da0e5c3
Emiliano Gandini Outeda <104856039+emiliano-gandini-outeda@users.noreply.github.com>
2026-05-26 11:40:13 -0300

    feat: data as a function

--- src/data.py
+++ src/data.py
@@ -2,52 +2,40 @@ import pandas as pd
 import requests
 import io
 
-"""
 
-Importing Data
+def run(output_path: str = "data/processed/auto_clean.csv") -> pd.DataFrame:
+    res = requests.get("http://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data").content
 
-For this project, I'll use a dataset from UC Irvine Machine Learning Repository,
-specifically, http://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data .
+    auto = pd.read_csv(io.StringIO(res.decode("utf-8")), header=None, na_values="?")
 
-"""
+    auto.columns = ['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors',
+                    'body-style', 'drive-wheels', 'engine-location', 'wheel-base', 'length', 'width', 'height',
+                    'curb-weight', 'engine-type', 'num-of-cylinders', 'engine-size', 'fuel-system', 'bore',
+                    'stroke', 'compression-ratio', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
 
-res = requests.get("http://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data").content
+    auto.dropna(subset=['price'], inplace=True)
 
-auto = pd.read_csv(io.StringIO(res.decode("utf-8")), header=None, na_values="?")
+    print(auto.isnull().sum().sort_values(ascending=False))
 
-auto.columns = ['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors',
-                            'body-style', 'drive-wheels', 'engine-location', 'wheel-base', 'length', 'width', 'height',
-                            'curb-weight', 'engine-type', 'num-of-cylinders', 'engine-size', 'fuel-system', 'bore',
-                            'stroke', 'compression-ratio', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
+    auto.drop(columns=['normalized-losses'], inplace=True)
 
+    numeric_cols = auto.select_dtypes(include='number').columns
+    auto[numeric_cols] = auto[numeric_cols].fillna(auto[numeric_cols].median())
 
-# Drop rows where price is missing
-auto.dropna(subset=['price'], inplace=True)
+    categorical_cols = auto.select_dtypes(include='object').columns
+    auto[categorical_cols] = auto[categorical_cols].fillna(auto[categorical_cols].mode().iloc[0])
 
-# Check for almost empty rows
-print(auto.isnull().sum().sort_values(ascending=False))
+    auto.drop_duplicates(inplace=True)
 
-# Drop them
-auto.drop(columns=['normalized-losses'], inplace=True)
+    categorical_cols = ['make', 'fuel-type', 'aspiration', 'num-of-doors',
+                        'body-style', 'drive-wheels', 'engine-location',
+                        'engine-type', 'num-of-cylinders', 'fuel-system']
 
-# fill numeric NaNs with median (robust to outliers)
-numeric_cols = auto.select_dtypes(include='number').columns
-auto[numeric_cols] = auto[numeric_cols].fillna(auto[numeric_cols].median())
+    auto = pd.get_dummies(auto, columns=categorical_cols, drop_first=True)
 
-# fill categorical NaNs with mode
-categorical_cols = auto.select_dtypes(include='str').columns
-auto[categorical_cols] = auto[categorical_cols].fillna(auto[categorical_cols].mode().iloc[0])
-
-# Remove duplicates
-auto.drop_duplicates(inplace=True)
+    auto.to_csv(output_path, index=False)
+    return auto
 
 
-categorical_cols = ['make', 'fuel-type', 'aspiration', 'num-of-doors',
-                    'body-style', 'drive-wheels', 'engine-location',
-                    'engine-type', 'num-of-cylinders', 'fuel-system']
-
-auto = pd.get_dummies(auto, columns=categorical_cols, drop_first=True)
-
-
-## Save to CSV
-auto.to_csv('data/processed/auto_clean.csv', index=False)
+if __name__ == "__main__":
+    run()
